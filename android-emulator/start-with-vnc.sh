864848fe983334012eaa9c046ac2aac5bf958b1e
Swind <swind@cloudmosa.com>
2019-10-09 16:23:06 +0800

    Add xvfb and x11vnc (Thanks budtmo/docker-android)

--- android-emulator/start-with-vnc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-ANDROID_EMULATOR_EXTRA_ARGS="-skin 1080x1920 -memory 2048 -gpu guest -no-boot-anim -qemu -vnc :0"
-ANDROID_AVD_EXTRA_ARGS="--device 8 --force"
-docker run --privileged -it -e ANDROID_AVD_EXTRA_ARGS="${ANDROID_AVD_EXTRA_ARGS}" -e ANDROID_EMULATOR_EXTRA_ARGS="${ANDROID_EMULATOR_EXTRA_ARGS}" -e noVNC="true" --rm $1
---
+++ android-emulator/start.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#ANDROID_EMULATOR_EXTRA_ARGS="-skin 1080x1920 -memory 2048 -gpu guest -no-boot-anim -qemu -vnc :0"
+ANDROID_EMULATOR_EXTRA_ARGS="-skin 1080x1920 -memory 2048 -gpu host -no-boot-anim -qemu"
+ANDROID_AVD_EXTRA_ARGS="--device 8 --force"
+docker run --privileged -it -e ANDROID_AVD_EXTRA_ARGS="${ANDROID_AVD_EXTRA_ARGS}" -e ANDROID_EMULATOR_EXTRA_ARGS="${ANDROID_EMULATOR_EXTRA_ARGS}" --name android-emulator --rm $1
