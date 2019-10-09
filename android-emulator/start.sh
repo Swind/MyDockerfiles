2269a507c14fe5c9b302fd8fe2081de6a6681198
Swind <swind@cloudmosa.com>
2019-10-09 17:11:17 +0800

    Add android 26, 27, 28, 29

--- android-emulator/start.sh
+++ android-emulator/start.sh
@@ -3,4 +3,4 @@
 #ANDROID_EMULATOR_EXTRA_ARGS="-skin 1080x1920 -memory 2048 -gpu guest -no-boot-anim -qemu -vnc :0"
 ANDROID_EMULATOR_EXTRA_ARGS="-skin 1080x1920 -memory 2048 -gpu host -no-boot-anim -qemu"
 ANDROID_AVD_EXTRA_ARGS="--device 8 --force"
-docker run --privileged -it -e ANDROID_AVD_EXTRA_ARGS="${ANDROID_AVD_EXTRA_ARGS}" -e ANDROID_EMULATOR_EXTRA_ARGS="${ANDROID_EMULATOR_EXTRA_ARGS}" --name android-emulator --rm $1
+docker run --privileged -it -p 6080:6080 -e ANDROID_AVD_EXTRA_ARGS="${ANDROID_AVD_EXTRA_ARGS}" -e ANDROID_EMULATOR_EXTRA_ARGS="${ANDROID_EMULATOR_EXTRA_ARGS}" --name android-emulator --rm $1
